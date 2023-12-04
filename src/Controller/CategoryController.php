1b69cb8e56d8775cb42ca3f96446402d7bf9a888
HMIMID DRISS p2106555 <driss.hmimid@etu.univ-lyon1.fr>
2023-12-04 09:20:05 +0100

    [ADD] - weapon's page

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CategoryController.php',
+        $listWeapons = ['arme1','arme2','arme3','arme4','arme5'];
+
+        return $this->render('category/index.html.twig',[
+            'controller_name' => 'CategoryController',
+            'listWeapons' => $listWeapons
         ]);
     }
 }
